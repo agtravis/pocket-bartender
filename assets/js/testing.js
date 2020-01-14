420660967dee95b628b1fe2753ec00ab615ee212
George Travis <agtravis85@gmail.com>
2020-01-14 15:23:25 -0800

    small nutrition stats work

--- assets/js/testing.js
+++ assets/js/testing.js
@@ -385,12 +385,42 @@ function makeDrinks(whichDrink, containerNumber) {
         drinkCalories.style.display = 'block';
       }
     });
+
+  document
+    .getElementById('image' + containerNumber + 'SM')
+    .addEventListener('click', function() {
+      var drinkName = drinksArray[0][containerNumber].name;
+      var getDrinkName = document.getElementById(
+        'drink-container-' + containerNumber
+      ).childNodes[1].textContent;
+      var getIngredients = drinksArray[0][containerNumber].ingredients;
+
+      if (getDrinkName === drinkName) {
+        for (var i = 0; i < getIngredients.length; i++) {
+          var ingredientsKeyword = getIngredients[i];
+          nutritionTest(ingredientsKeyword, containerNumber);
+        }
+      }
+
+      if (
+        drinkRecipes.style.display === 'block' &&
+        drinkIngredients.style.display === 'block' &&
+        drinkCalories.style.display === 'block'
+      ) {
+        drinkRecipes.style.display = 'none';
+        drinkIngredients.style.display = 'none';
+        drinkCalories.style.display = 'none';
+      } else {
+        drinkRecipes.style.display = 'block';
+        drinkIngredients.style.display = 'block';
+        drinkCalories.style.display = 'block';
+      }
+    });
 }
 
 document
   .getElementById('information-containerSM')
   .addEventListener('click', function(event) {
-    console.log(event.target);
     var i = event.target.id;
     i = i.charAt(5);
     if (event.target.matches('img') || event.target.matches('p')) {
