5895beaa95ec4fbc9d70aadca7f3862dac52850b
George Travis <wewerelikesharks@gmail.com>
2020-01-18 08:10:09 -0800

    removed comments except where indicates importand DO NOT REMOVE instance of greyed out variable

--- assets/js/testing.js
+++ assets/js/testing.js
@@ -1,16 +1,12 @@
-//variables stored are the key, the starting point for the query URL
 var liquorCabinet = [];
 var liquorImageLinks = [];
 var drinksArray;
 var drinksObj;
 
-// globally accessible current item - undefined on page load, defined as soon as it is needed, and changes
 var currentItem = '';
 var apikey = '9973533';
 var apiaddress = 'https://www.thecocktaildb.com/api/json/v2/' + apikey + '/';
 
-//The input field and the search button
-//I have only stored 2 elements as variables. To clean up the code I can replace each 'document.getElementById' with variables
 var userInput = document.getElementById('search');
 var searchButton = document.getElementById('search-button');
 var searchButtonSM = document.getElementById('search-buttonSM');
@@ -34,12 +30,8 @@ var drinksObj;
 var userInstructions = document.getElementById('user-instructions');
 var userInstructionsSM = document.getElementById('user-instructionsSM');
 
-//populates liquor cabinet on page load from local storage
 initialize();
 
-//EVENT LISTENERS
-
-//the modal opens whether the user hits ENTER or clicks the button, passing the input
 searchButton.addEventListener('click', function() {
   openModal(userInput.value);
 });
@@ -57,8 +49,6 @@ userInputSM.addEventListener('keyup', function(event) {
   }
 });
 
-//when the liquor cabinet is clicked, if the item clicked is an image, the modal opens passing the element name
-//evaluated by slicing the dynamically generated id name
 liquorCabinetDiv.addEventListener('click', function(event) {
   var element = event.target;
   if (element.matches('img')) {
@@ -74,7 +64,6 @@ liquorCabinetDivSM.addEventListener('click', function(event) {
   }
 });
 
-//if the user ran out, this removes it from local storage and hence the cabinet
 statusInButton.addEventListener('click', function() {
   modalElement.classList.add('hide');
   informationContainer.classList.remove('opaque');
@@ -84,7 +73,6 @@ statusInButton.addEventListener('click', function() {
   initialize();
 });
 
-//if the user adds to their cabinet, this function runs
 statusOutButton.addEventListener('click', function() {
   modalElement.classList.add('hide');
   informationContainer.classList.remove('opaque');
@@ -110,14 +98,12 @@ statusOutButton.addEventListener('click', function() {
   }
 });
 
-//button to close custom alert pop up
 modalAlertButton.addEventListener('click', function() {
   modalAlertElement.classList.add('hide');
   informationContainer.classList.remove('opaque');
   informationContainerSM.classList.remove('opaque');
 });
 
-//regardless of inventory status, this is where the user chooses to display recipes/facts etc.
 displayInfoButton.addEventListener('click', function() {
   modalElement.classList.add('hide');
   informationContainer.classList.remove('opaque');
@@ -125,14 +111,12 @@ displayInfoButton.addEventListener('click', function() {
   searchIngredient(currentItem);
 });
 
-//just closes the modal without doing anything
 cancelButton.addEventListener('click', function() {
   informationContainer.classList.remove('opaque');
   informationContainerSM.classList.remove('opaque');
   modalElement.classList.add('hide');
 });
 
-//for loop add event listeners on ingredients list to be able to target words
 for (var i = 1; i <= 3; ++i) {
   document
     .getElementById('ingredients' + [i])
@@ -146,7 +130,6 @@ for (var i = 1; i <= 3; ++i) {
     });
 }
 
-//toggles opacity on coasters to show recipe on click, accounts for paragraph obscuring image
 informationContainerSM.addEventListener('click', function(event) {
   var i = event.target.id;
   i = i.charAt(5);
@@ -165,9 +148,6 @@ informationContainerSM.addEventListener('click', function(event) {
   }
 });
 
-//FUNCTION DECLARATIONS
-
-//fills the array from local storage (called on page load)
 function initialize() {
   liquorCabinet = JSON.parse(localStorage.getItem('liquor-cabinet'));
   if (liquorCabinet) {
@@ -179,15 +159,12 @@ function initialize() {
         '-Small.png';
       liquorImageLinks.push(imageURL);
     }
-    // console.log(liquorCabinet);
-    // console.log(liquorImageLinks);
   } else {
     liquorCabinet = [];
   }
   renderLiquorCabinet();
 }
 
-//displays visual representation of the liquor cabinet array
 function renderLiquorCabinet() {
   liquorCabinetDiv.innerHTML = '';
   liquorCabinetDivSM.innerHTML = '';
@@ -203,7 +180,6 @@ function renderLiquorCabinet() {
   }
 }
 
-//custom alert pop-up
 function modalAlert(message) {
   modalMessage.textContent = message;
   modalAlertElement.classList.remove('hide');
@@ -211,8 +187,6 @@ function modalAlert(message) {
   informationContainerSM.classList.add('opaque');
 }
 
-//sets which buttons or text to display based on inventory status
-//also this is the point where 'currentItem' is assigned (for global use)
 function openModal(item) {
   if (item) {
     item = item.toLowerCase();
@@ -243,7 +217,6 @@ function openModal(item) {
   }
 }
 
-//first AJAX call looks for ingredient(s), returns object 'response', calls helper function passing response
 function searchIngredient(userChoice) {
   var userIngredient = userChoice;
   var queryURL = apiaddress + 'filter.php?i=' + userIngredient;
@@ -262,7 +235,6 @@ function searchIngredient(userChoice) {
   xmlhttp.send();
 }
 
-//displays drink name and image, uses drink ID to now call makeDrinks function
 function displayDrink(response) {
   userInstructions.classList.add('hide');
   userInstructionsSM.classList.add('hide');
@@ -338,7 +310,6 @@ function displayDrink(response) {
   }
 }
 
-//next AJAX call uses drink ID to get recipe and ingredients, helper function called to parse ingredients
 function getRecipe(drinkId, i) {
   var queryURLsub = apiaddress + 'lookup.php?i=' + drinkId;
   var xmlhttpsub = new XMLHttpRequest();
@@ -356,9 +327,6 @@ function getRecipe(drinkId, i) {
   xmlhttpsub.send();
 }
 
-//fills arrays with ingredients and measures. Could also do with a single array of anonymous objects.
-//accounts for null values. condition is <=15 because each drink response has this many potential ingredients
-//code kind of clunky inside for loops, perhaps can be streamlined - not sure of scope for iterators?
 function fillIngredients(response, currentDrink) {
   //VS CODE shows response here as not called but it is....
   var ingredients = [];
@@ -400,11 +368,6 @@ function fillIngredients(response, currentDrink) {
   drinksArray[0][currentDrink]['measures'] = measures;
 }
 
-// creates the MAKE DRINK button and hides the drink information aside from the picture
-// calls getRecipe()
-// calls getNutrition() on click, which gets the value for calories for each ingredient (if it can find it)
-// totals up the calories and adds it to the DOM
-// clicking the button again will hide the elements
 function makeDrinks(whichDrink, containerNumber) {
   var drinkBtn = document.createElement('button');
   var drinkContainer = document.getElementById(
@@ -477,14 +440,6 @@ function makeDrinks(whichDrink, containerNumber) {
             nutritionTest(ingredientsKeyword, containerNumber);
           }
         }
-
-        // if (
-        //   drinkInfoSM.style.display === 'block'
-        // ) {
-        //   drinkInfoSM.style.display = 'none';
-        // } else {
-        //   drinkInfoSM.style.display = 'block';
-        // }
       }
     });
 }
